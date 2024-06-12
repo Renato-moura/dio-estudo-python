9779d9ea2c9d0bb4918f67641b00492a18f4c3ba
Renato-moura <renatomoura.ms@gmail.com>
2024-06-12 00:03:23 -0300

    primeiras aulas

---
+++ aulas/comparacao.py
@@ -0,0 +1,8 @@
+saldo = 100
+saque = 200
+
+print (saldo == saque) #saldo igual saque - não (false)
+print (saldo != saque) #saldo diferente de saque - sim (true)
+print (saldo >= saque) #saldo maior igual a saque - não (false)
+print (saldo <= saque) #saldo menor igual a saque - sim (true)
+   
---
+++ aulas/operadores.py
@@ -0,0 +1,7 @@
+print(1+1)
+print(1-1)
+print(2*3)
+print(2**3) #exponencial
+print(10/3) #divisão fracionada
+print(10//3) #divisão inteira
+print(10%2) #modulo da divisão
